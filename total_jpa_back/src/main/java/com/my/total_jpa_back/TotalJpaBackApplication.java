c9fd482d1001f0e925c14f2668d8bd439a0de927
zzzmini <zzzmini@gmail.com>
2026-05-27 09:45:24 +0900

    feat : first commit

---
+++ total_jpa_back/src/main/java/com/my/total_jpa_back/TotalJpaBackApplication.java
@@ -0,0 +1,13 @@
+package com.my.total_jpa_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalJpaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalJpaBackApplication.class, args);
+	}
+
+}
---
+++ total_jpa_back/src/test/java/com/my/total_jpa_back/TotalJpaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.total_jpa_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalJpaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
